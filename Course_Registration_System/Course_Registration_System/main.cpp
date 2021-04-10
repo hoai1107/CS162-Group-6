1ae22dc575b30fd71bbc6bc0d79b30d9797651fe
phipk02 <phipk02@gmail.com>
2021-04-10 14:27:02 +0700

    Edit function login

--- Course_Registration_System/Course_Registration_System/main.cpp
+++ Course_Registration_System/Course_Registration_System/main.cpp
@@ -7,11 +7,12 @@ int main() {
 
 	// After load data.
 	int res = 0;
+	int indexOfUser;
 	while (res != 1 && res != 2) {
 		gotoxy(55, 1);
 		cout << "LOGIN";
 		// APPEARANCE OF LOGIN SYSTEM
-		res = login(_staff, _student);
+		res = login(_staff, _student, indexOfUser);
 
 		gotoxy(47, 5);
 		if (res == 0) {
