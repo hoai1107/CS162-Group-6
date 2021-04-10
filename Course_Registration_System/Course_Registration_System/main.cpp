e8b0a34a3c35269fb843a7d3ffe006d75521ff1a
phipk02 <phipk02@gmail.com>
2021-04-10 10:57:25 +0700

    Edit main.cpp

--- Course_Registration_System/Course_Registration_System/main.cpp
+++ Course_Registration_System/Course_Registration_System/main.cpp
@@ -1,6 +1,32 @@
 #include "Function.h"
 
 int main() {
+	SetConsoleOutputCP(65001); // Be able to output Vietnamese Words
+	vector<staff> _staff;
+	vector<student> _student;
 
+	// After load data.
+	int res = 0;
+	while (res != 1 && res != 2) {
+		gotoxy(55, 1);
+		cout << "LOGIN";
+		// APPEARANCE OF LOGIN SYSTEM
+		res = login(_staff, _student);
+
+		gotoxy(47, 5);
+		if (res == 0) {
+			cout << "Wrong ID or password!";
+			Sleep(1000);
+		}
+		system("CLS");
+	}
+
+	if (res == 1) {
+		// STAFF
+	}
+
+	else {
+		// STUDENT
+	}
 	return 0;
 }
