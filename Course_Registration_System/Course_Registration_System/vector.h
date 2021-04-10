17b37d6485972e09622e5eb7e3f39845daae0af5
leviosarz <dungdq2002@gmail.com>
2021-04-10 11:08:28 +0700

    add function "push_back" to vector.h
    [vector_name].push_back(new_value) to insert new_value at the end of vector_name

--- Course_Registration_System/Course_Registration_System/vector.h
+++ Course_Registration_System/Course_Registration_System/vector.h
@@ -1,18 +1,34 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <stdexcept>
+
 template <typename T>
 struct vector {
 	T* array;
 	int _size = 0;
-	int _capacity = 0;
+	int _capacity = 1;
+
+	vector() : array(new T[_capacity]) {}
 
 	int size() {
 		return _size;
 	}
 
-	void push_back(T _x);
+	void push_back(T _x) {
+		if (_size == _capacity) {
+			T* tmp = new T[_capacity * 2];
+			for (int i = 0; i < _capacity; i++)
+				tmp[i] = array[i];
+			array = tmp;
+			_capacity *= 2;
+		}
+		array[_size++] = _x;
+		return;
+	}
+
 	void erase(int _index);
+
 	T operator[](int _index);
 };
 #endif
