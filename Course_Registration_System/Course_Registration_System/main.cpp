1aa584030373afc3801b60342fd81d13e7fe0df7
phipk02 <phipk02@gmail.com>
2021-04-22 19:41:56 +0700

    Edit main

--- Course_Registration_System/Course_Registration_System/main.cpp
+++ Course_Registration_System/Course_Registration_System/main.cpp
@@ -2,11 +2,28 @@
 
 int main() {
 	SetConsoleOutputCP(65001); // Be able to output Vietnamese Words
-	vector<staff> _staff;
-	vector<student> _student;
-	semester _semester;
-
+	Vector<staff> _staff;
+	Vector<student> _student;
+	Vector<semester> _semester;
+	semester nSemester;
 	// After load data.
+
+	struct tm newtime;
+	time_t now = time(0);
+	localtime_s(&newtime, &now);
+
+	date today;
+	today.day = newtime.tm_mday;
+	today.month = newtime.tm_mon + 1;
+	today.year = newtime.tm_year + 1900;
+
+	for (int i = 0; i < _semester.size(); i++)
+		if (_semester[i].startDate <= today && today <= _semester[i].endDate) {
+			nSemester = _semester[i];
+			break;
+		}
+
+
 	int res = 0;
 	int indexOfUser = 0;
 	while (res != 1 && res != 2) {
@@ -29,17 +46,8 @@ int main() {
 
 	else {
 
-		struct tm newtime;
-		time_t now = time(0);
-		localtime_s(&newtime, &now);
-
-		date today;
-		today.day = newtime.tm_mday;
-		today.month = newtime.tm_mon + 1;
-		today.year = newtime.tm_year + 1900;
-
-		if (_semester.regOpen <= today && today <= _semester.regClose) {
-			vector<string> allActions; 
+		if (nSemester.regOpen <= today && today <= nSemester.regClose) {
+			Vector<string> allActions; 
 			allActions.push_back("Enroll in a course.");
 			allActions.push_back("View list of enrolled courses.");
 			allActions.push_back("Remove a course from the enrolled list.");
@@ -48,28 +56,28 @@ int main() {
 			bool exit = false;
 			do {
 				cout << "ENTER to go into tasks. UP ARROW, DOWN ARROW to move. BACKSPACE to exit.";
-				choice = actionList(allActions, allActions.size(), {0, 1});
+				choice = actionList(allActions, {0, 1});
 				switch (choice) {
 					system("CLS");
 					case 0: {
-						enrollCourses(_student[indexOfUser], _semester);
+						enrollCourses(_student[indexOfUser], nSemester);
 						break;
 					}
 					case 1: {
-						viewEnrolledCourses(_student[indexOfUser], _semester);
+						viewEnrolledCourses(_student[indexOfUser], nSemester);
 						break;
 					}
 					case 2: {
-						removeCourseFromList(_student[indexOfUser], _semester);
+						removeCourseFromList(_student[indexOfUser], nSemester);
 						break;
 					}
 					case 3:
 					case 4: {
 						cout << "Do you really want to exit?";
-						vector<string> confirm;
+						Vector<string> confirm;
 						confirm.push_back("YES");
 						confirm.push_back("NO");
-						int t = actionList(confirm, 2, {0, 1});
+						int t = actionList(confirm, {0, 1});
 						if (t == 0) exit = true;
 						else exit = false;
 						break;
@@ -78,18 +86,18 @@ int main() {
 			} while (!exit);
 		}
 		else {
-			vector<string> allActions; 
+			Vector<string> allActions; 
 			allActions.push_back("View list of your courses.");
 			allActions.push_back("View your scoreboard.");
 			int choice;
 			bool exit = false;
 			do {
 				cout << "ENTER to go into tasks. UP ARROW, DOWN ARROW to move. BACKSPACE to exit.";
-				choice = actionList(allActions, allActions.size(), {0, 1});
+				choice = actionList(allActions, {0, 1});
 				switch (choice) {
 					system("CLS");
 					case 0: {
-						viewEnrolledCourses(_student[indexOfUser], _semester);
+						viewEnrolledCourses(_student[indexOfUser], nSemester);
 						break;
 					}
 					case 1: {
@@ -99,10 +107,10 @@ int main() {
 					case 2:
 					case 3: {
 						cout << "Do you really want to exit?";
-						vector<string> confirm;
+						Vector<string> confirm;
 						confirm.push_back("YES");
 						confirm.push_back("NO");
-						int t = actionList(confirm, 2, {0, 1});
+						int t = actionList(confirm, {0, 1});
 						if (t == 0) exit = true;
 						else exit = false;
 						break;
